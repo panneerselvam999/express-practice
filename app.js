c65cf61c266abc3357f8bd1ab2226793025e43ae
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-02-03 16:00:28 -0500

    set 404.html file

--- app.js
+++ app.js
@@ -232,6 +232,7 @@
 const express = require("express");
 const app = express();
 const bp = require("body-parser");
+const path = require("path");
 const PORT = 3500;
 
 const adminRoute = require("./routes/admin");
@@ -241,7 +242,8 @@ const shopRoute = require("./routes/shop");
 app.use(adminRoute);
 app.use(shopRoute);
 app.use((req, res, next) => {
-  res.status(404).send("<h1>404 error</h1>");
+  // res.status(404).send("<h1>404 error</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 app.use(bp.urlencoded());
 
